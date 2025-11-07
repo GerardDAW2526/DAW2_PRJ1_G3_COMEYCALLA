fc1daac6a3c9ff314c321d14d55047f01534cb36
GerardDAW2526 <446900.joan23@fje.edu>
2025-11-07 16:56:41 +0100

    estructura_general_v1

---
+++ view/principal.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Come & Calla</title>
+</head>
+<body>
+    
+    <!-- El contenedor que envuelve toda la estructura -->
+
+    <div class="contenedor-principal">
+
+
+        <!-- Header, dónde irá el botón de iniciar sesión -->
+        <div class="header">
+
+
+            <!-- Contenedor del botón -->
+
+            <div class="btn">
+
+
+                <!-- Botón de iniciar sesión -->
+
+                <a href="btn-login">Iniciar sesión</a>
+
+            </div>
+
+
+        </div>
+
+        <!-- Contenedor del contenido -->
+        <div class="content">
+
+            <!-- Contenedor dónde irá la imagen -->
+            <div class="img-content">
+                <img src="" alt="No se ha podido cargar la imagen">
+            </div>
+
+
+            <!-- Contenedor dónde irá el texto -->
+            <div class="text-content">
+                <p>Nuestras excusas vienen con guarnición.</p>
+            </div>
+
+
+        </div>
+
+    </div>
+
+
+    
+
+
+    <!-- Archivo less (una extension de CSS) -->
+    <link rel="stylesheet/less" type="text/css" href="../estilos/estilos_primario.less" />
+
+    <!-- Recogida del código de la página principal de less (su configuración para su respectivo funcionamiento) -->
+    <script src="https://cdn.jsdelivr.net/npm/less" ></script>
+
+</body>
+</html>
