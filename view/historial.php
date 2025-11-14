b98db9db45ce47c64fcfd01386511f5b23387f23
jesusfortea <464186.joan23@fje.edu>
2025-11-14 15:42:58 +0100

    Todo acabado, falta algún detalle de estilos y añadir extras

--- view/historial.php
+++ view/historial.php
@@ -108,6 +108,10 @@ $ocupaciones = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <script src="./../js/validaciones.js"></script>
     <link rel="stylesheet" href="../estilos/estilos_historial.css">
     <!-- <script src="../js/validaciones.js"></script> -->
+
+    <!-- LESS -->
+    <link rel="stylesheet/less" type="text/css" href="../estilos/estilos_salas.less" />
+    <script src="https://cdn.jsdelivr.net/npm/less"></script>
 </head>
 <body>
 <div class="header">
@@ -121,7 +125,7 @@ $ocupaciones = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         <div>
 
-            <a href="./historial.php"><img src="" alt="No se ha podido cargar la imagen"></a>
+            <a href="./historial.php" class="header-svg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"/></svg></a>
             <form action="./logout.php" method="post">
                 <button type="submit" class="btn-cerrar-sesion">Cerrar Sesión</button>
             </form>
@@ -264,10 +268,7 @@ $ocupaciones = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 </div>
 
-    
-    <!-- LESS -->
-    <link rel="stylesheet/less" type="text/css" href="../estilos/estilos_salas.less" />
-    <script src="https://cdn.jsdelivr.net/npm/less"></script>
+
 
 
 </body>
